a94c3940b0d173e7a10ae0a4b6aa1b70a20174da
jisyed14@gmail.com <jisyed14@gmail.com@b86a2a93-6637-5542-a1a1-0283ad3f4825>
2012-10-01 23:10:47 +0000

    Fixed MissingReference Error on Prism's destruction

--- trunk/Reveal/Assets/Scripts/Camera_Follow_Player.cs
+++ trunk/Reveal/Assets/Scripts/Camera_Follow_Player.cs
@@ -4,9 +4,18 @@ using System.Collections;
 public class Camera_Follow_Player : MonoBehaviour {
 	public GameObject player;
 	void Update () {
-		Vector3 newPosition = transform.position;
+			Vector3 newPosition = transform.position;	
+		if(!(player))
+		{
+			newPosition.x = 0;
+			newPosition.z = 0;
+			newPosition.y = 150;
+		}
+		else
+		{
 		newPosition = player.transform.position;
 		newPosition.y = player.transform.position.y + 50;
+		}
 		transform.position = newPosition;
 	}
 }
