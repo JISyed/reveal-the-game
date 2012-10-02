1ecfaf578c26ec00c6097d5b7dd5ddb4d2713fd8
jisyed14 <jisyed14@gmail.com>
2012-10-02 04:46:04 +0000

    Inplemented the swtiching of state between an activated beacon and deactivated beacon

--- Reveal/Assets/Scripts/Destroy_Collide_Wall.cs
+++ Reveal/Assets/Scripts/Destroy_Collide_Wall.cs
@@ -7,7 +7,14 @@ public class Destroy_Collide_Wall : MonoBehaviour {
 	
 	public GameObject player;
 	// public string levelName;
-	// Use this for initialization
+	
+	void Start()
+	{
+	}
+	
+	void Update()
+	{
+	}
 	
 	void OnCollisionEnter(Collision other)
 	{
@@ -18,6 +25,18 @@ public class Destroy_Collide_Wall : MonoBehaviour {
 		//if(other.gameObject == player) /// This if statement does not work!
 		//{
 			Manage_Game.numOfLives -= 1;
+			if(Manage_Game.numOfLives > 0)
+			{
+				Invoke("RevivePrism", 3);
+			}
 		//}
+	}
+	
+	void RevivePrism()
+	{
+		// Bad coding practice!!!
+		// Needs to be more dynamic on a level-to-level basis.
+		Vector3 prismPos = new Vector3(-87.0f, 7.36f, 36.0f);
+		Instantiate(player, prismPos, player.transform.rotation);
 	}
 }
