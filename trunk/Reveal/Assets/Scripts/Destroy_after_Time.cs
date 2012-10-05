a28c0be09a79b1aa6d7e96fe10964c6d3ff33f1d
jisyed14@gmail.com <jisyed14@gmail.com@b86a2a93-6637-5542-a1a1-0283ad3f4825>
2012-10-05 21:06:26 +0000

    Implemented beacon timeout (deactivationTimeInSeconds inside Scripts/Change_Beacon_State.cs) and also added a cool flickering effect in the last 3 seconds of activation.
    Also made flickering work if deactivationTimeInSeconds was less than 3.

--- trunk/Reveal/Assets/Scripts/Destroy_after_Time.cs
+++ trunk/Reveal/Assets/Scripts/Destroy_after_Time.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 using System.Collections;
 
 public class Destroy_after_Time : MonoBehaviour {
-
+	
+	// Will not do anything if 0
 	public float timeInSeconds = 10.0f;
 	
 	// Use this for initialization
 	void Start () {
-		Destroy(gameObject, timeInSeconds);
+		if(timeInSeconds > 0)
+		{
+			Destroy(gameObject, timeInSeconds);
+		}
 	}
 	
 	// Update is called once per frame
