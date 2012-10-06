25860e038ad2ea29d3054d0b0c274737d55199ea
jisyed14@gmail.com <jisyed14@gmail.com@b86a2a93-6637-5542-a1a1-0283ad3f4825>
2012-10-06 17:54:29 +0000

    Now boosting only work if holding shift. Letting go of shift should stop boosting. Also implemented a Start screen state, where the game pauses before pressing enter to start the game.

--- trunk/Reveal/Assets/Scripts/Player_Boost.cs
+++ trunk/Reveal/Assets/Scripts/Player_Boost.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class Player_Boost : MonoBehaviour {
 	
 	public static bool onThrust = false;
+	
+	public float boostMultiplier = 2.5f;
+	public float defaultSlowSpeed = 700.0f;
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -12,25 +16,31 @@ public class Player_Boost : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if((Input.GetKeyDown(KeyCode.LeftShift)) || (Input.GetKeyDown (KeyCode.RightShift)))
+		if((Input.GetKeyDown(KeyCode.LeftShift)) || (Input.GetKeyDown(KeyCode.RightShift)))
 		{
 			if(onThrust)
 			{
-				Move_360.thrustSpeed = 700f;
+				Move_360.thrustSpeed = defaultSlowSpeed;
 				onThrust = false;
 			}
 			else
 			{
-				Move_360.thrustSpeed *= 2;
+				Move_360.thrustSpeed *= boostMultiplier;
 				onThrust = true;
 			}
 			
 			if(Manage_Game.lightCount <= 0)
 			{
-				Move_360.thrustSpeed = 700f;
+				Move_360.thrustSpeed = defaultSlowSpeed;
 				onThrust = false;	
 			}
-				
+			
+		}
+		
+		if( (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift)) && onThrust)
+		{
+			Move_360.thrustSpeed = defaultSlowSpeed;
+			onThrust = false;
 		}
 	}
 }
