8ca8f70e5d69823012da5092405c860c6ed0f954
jisyed14@gmail.com <jisyed14@gmail.com@b86a2a93-6637-5542-a1a1-0283ad3f4825>
2012-10-12 17:54:18 +0000

    Implemented boosting and idle particles for the prism. However, there is a bug where both boosting sound and boosting particles are still active if you press down shift after light energy is depleted.

---
+++ trunk/Reveal/Assets/Scripts/Toggle_Particle_Emission.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+// Credit to profanicus
+// http://forum.unity3d.com/threads/118117-Starting-and-Stopping-Emitters-in-the-new-Shuriken-Particle-System?p=791969&viewfull=1#post791969
+
+public class Toggle_Particle_Emission : MonoBehaviour {
+	
+	public static ParticleSystem myParticles;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		myParticles = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButtonDown("Fire2"))
+		{
+			myParticles.Play();
+		}
+		
+		if(Input.GetButtonUp("Fire2"))
+		{
+			myParticles.Stop();
+		}
+	}
+}
