7eb16d5c6e5d69ce9ad03d902b9a43951490f778
jisyed14 <jisyed14@gmail.com>
2012-10-12 17:54:18 +0000

    Implemented boosting and idle particles for the prism. However, there is a bug where both boosting sound and boosting particles are still active if you press down shift after light energy is depleted.

--- Reveal/Assets/Scripts/Player_Boost.cs
+++ Reveal/Assets/Scripts/Player_Boost.cs
@@ -8,29 +8,26 @@ public class Player_Boost : MonoBehaviour {
 	public float boostMultiplier = 2.5f;
 	public float defaultSlowSpeed = 700.0f;
 	
-	// Use this for initialization
-	void Start () {
+	private ParticleSystem boostParticles;
 	
+	// Use this for initialization
+	void Start () 
+	{
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
 		// Fire2 is defined in InputManager
 		if(Input.GetButtonDown("Fire2"))
 		{
-			if(onThrust)
-			{
-				Move_360.thrustSpeed = defaultSlowSpeed;
-				onThrust = false;
-				gameObject.audio.Stop();
-				gameObject.audio.time = 0.0f;
-			}
-			else
+			if(!onThrust)
 			{
 				Move_360.thrustSpeed *= boostMultiplier;
 				onThrust = true;
 				gameObject.audio.Play();
+				
 			}
 			
 			if(Manage_Game.lightCount <= 0)
