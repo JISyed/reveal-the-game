e531814132dc2fe839004306f135d02f1700d99b
shoover56@comcast.net <shoover56@comcast.net@b86a2a93-6637-5542-a1a1-0283ad3f4825>
2012-10-25 17:54:36 +0000

    -Added Power Up CS file to prepare implentation of powerups

---
+++ trunk/Reveal/Assets/Scripts/Power_Up_Management.cs
@@ -0,0 +1,148 @@
+using UnityEngine;
+using System.Collections;
+
+public class Power_Up_Management : MonoBehaviour {
+	
+	float tempPuNitro;
+	float tempPuJuice;
+	float tempEcoBoost;
+	float tempBeamGatling;
+	// Use this for initialization
+	void Start () {
+		pu_ExtraLife();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Manage_Game.lightCount < 3)
+		{
+			pu_BeamGatlingDisable();
+			pu_EcoBoostDisable();
+		}
+		
+	}
+	void CallValue(int i)
+	{
+		switch(i)
+		{
+		case 1:
+			pu_PrismCell();
+			break;
+		case 2:
+			pu_ExtraLife();
+			break;
+		case 3:
+			pu_Nitro();
+			break;
+		case 4:
+			pu_TriBeam();
+			break;
+		case 5:
+			pu_Energizer();
+			break;
+		case 6:
+			pu_Rocket(); //pause it
+			break;
+		case 7:
+			pu_Aura();
+			break;
+		case 8:
+			pu_Spotlight();
+			break;
+		case 9:
+			pu_BeamCannon();
+			break;
+		case 10:
+			pu_BeamGatling();
+			break;
+		case 11:
+			pu_EcoBoost();
+			break;
+		case 12:
+			pu_Juice();
+			break;
+		default:
+			pu_Nitro();
+			break;
+			
+		}
+	}
+	void pu_PrismCell()
+	{
+		
+	}
+	void pu_ExtraLife()
+	{
+		Manage_Game.numOfLives++;
+	}
+	void pu_Nitro()
+	{
+		tempPuNitro = Manage_Game.playerBoostCost;
+		Manage_Game.playerBoostCost = 0.0f;
+		Invoke ("pu_NitroDisable", 15.0f);
+	}
+	void pu_TriBeam()
+	{
+		
+	}
+	void pu_Energizer()
+	{
+		
+	}
+	void pu_Rocket()
+	{
+		
+	}
+	void pu_Aura()
+	{
+		
+	}
+	void pu_Spotlight()
+	{
+		
+	}
+	void pu_BeamCannon()
+	{
+		
+	}
+	void pu_BeamGatling()
+	{
+		isStillBeamGatling = true;
+		tempBeamGatling = Manage_Game.helixCost;
+		Manage_Game.helixCost /= 2;		
+	}
+	void pu_EcoBoost()
+	{
+		isStillEcoBoost = true;
+		tempEcoBoost = Manage_Game.playerBoostCost;
+		Manage_Game.playerBoostCost /= 2;
+	}
+	void pu_Juice()
+	{
+		tempPuJuice = Manage_Game.lightRegen;
+		if(Manage_Game.lightRegen ==0)
+			Manage_Game.lightRegen = 2.0f;	
+		Manage_Game.lightRegen *= 2;
+		Invoke ("pu_JuiceDisable", 30.0f);
+	}
+	
+	/* --------------------------------------------------------------------
+	 * DISABLE FUNCTIONS OVER TIME
+	 * --------------------------------------------------------------------*/
+	void pu_NitroDisable()
+	{
+		Manage_Game.playerBoostCost = tempPuNitro;
+	}
+	void pu_JuiceDisable()
+	{
+		Manage_Game.lightRegen = tempPuJuice;
+	}
+	void pu_BeamGatlingDisable()
+	{
+		Manage_Game.helixCost = tempBeamGatling;	
+	}
+	void pu_EcoBoostDisable()
+	{
+		Manage_Game.playerBoostCost = tempEcoBoost;
+	}
+}
