bd60c93553800764edb4b439abd1a93dd3871502
jisyed14 <jisyed14@gmail.com>
2012-10-27 01:40:04 +0000

    Started messing with arrows that get revealed by light.

---
+++ Reveal/Assets/Scripts/Reveal_Material_after_Lit.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class Reveal_Material_after_Lit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Concealed at first
+		renderer.material.color = Color.black;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
