941d5e6685d9e9a9e4a09e46d02b4da942688011
jisyed14 <jisyed14@gmail.com>
2012-10-28 01:11:02 +0000

    Implemented revealable arrows.

--- Reveal/Assets/Scripts/Reveal_Material_after_Lit.cs
+++ Reveal/Assets/Scripts/Reveal_Material_after_Lit.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 using System.Collections;
 
 public class Reveal_Material_after_Lit : MonoBehaviour {
-
+	
+	private bool revealed = false;
+	private bool alreadyRevealed = false;
+	
+	private float revealSpeed = 2.0f;
+	private float maxBrightRatio = 0.2f;
+	
 	// Use this for initialization
 	void Start () 
 	{
@@ -13,6 +19,26 @@ public class Reveal_Material_after_Lit : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		if(revealed && !alreadyRevealed)
+		{
+			renderer.material.color += Color.grey /revealSpeed * Time.deltaTime;
+		}
 		
+		if(renderer.material.color.r > maxBrightRatio)
+		{
+			alreadyRevealed = true;
+		}
+		
+	}
+	
+	// Collision
+	void OnTriggerEnter(Collider other)
+	{
+		// Turret_Bullet is the only unlit object that can collide with revealables
+		// because of Physics settings. So exclude it.
+		if(other.gameObject.name != "Turret_Bullet(Clone)")
+		{
+			revealed = true;
+		}
 	}
 }
