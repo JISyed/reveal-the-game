b162388f15a8b7584bf19f11b6650c0251c252d4
shoover56 <shoover56@comcast.net>
2012-11-06 18:24:10 +0000

    -Modified the light radius and got rid of the turning value's light regeneration disabling

--- Reveal/Assets/Scripts/Lock_Stuff/Change_Prism_Color.cs
+++ Reveal/Assets/Scripts/Lock_Stuff/Change_Prism_Color.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 using System.Collections;
 
 public class Change_Prism_Color : MonoBehaviour {
-
+	
+	private const float LIGHT_RANGE_VALUE = 35f;
+	private const float LIGHT_INTENSITY_VALUE = 0.65f;
 	// Use this for initialization
 	void Start () 
 	{
@@ -16,32 +18,32 @@ public class Change_Prism_Color : MonoBehaviour {
 		{
 			renderer.material.color = Manage_Game.col_white;
 			light.color = Manage_Game.col_white;
-			light.range = 25f;
-			light.intensity = 0.56f;
+			light.range = LIGHT_RANGE_VALUE;
+			light.intensity = LIGHT_INTENSITY_VALUE + 0.01f;
 		}
 		
 		if(Manage_Game.colorState == (int) Manage_Game.Colors.red)
 		{
 			renderer.material.color = Manage_Game.col_red;
 			light.color = Manage_Game.col_red;
-			light.range = 31f;
-			light.intensity = 0.59f;
+			light.range = LIGHT_RANGE_VALUE + 6f;
+			light.intensity = LIGHT_INTENSITY_VALUE + 0.04f;
 		}
 		
 		if(Manage_Game.colorState == (int) Manage_Game.Colors.green)
 		{
 			renderer.material.color = Manage_Game.col_green;
 			light.color = Manage_Game.col_green;
-			light.range = 29f;
-			light.intensity = 0.57f;
+			light.range = LIGHT_RANGE_VALUE + 4f;
+			light.intensity = LIGHT_INTENSITY_VALUE + 0.02f;
 		}
 		
 		if(Manage_Game.colorState == (int) Manage_Game.Colors.blue)
 		{
 			renderer.material.color = Manage_Game.col_blue;
 			light.color = Manage_Game.col_blue;
-			light.range = 34f;
-			light.intensity = 0.62f;
+			light.range = LIGHT_RANGE_VALUE + 9f;
+			light.intensity = LIGHT_INTENSITY_VALUE + 0.07f;
 		}
 	}
 }
