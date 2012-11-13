954e2e2918388416632effc2f2fafe31917cc981
shoover56 <shoover56@comcast.net>
2012-11-13 19:01:24 +0000

    -Fixed right Ctrl break

--- Reveal/Assets/Scripts/Apply_Checkpoint.cs
+++ Reveal/Assets/Scripts/Apply_Checkpoint.cs
@@ -48,7 +48,7 @@ public class Apply_Checkpoint : MonoBehaviour {
 	void Update()
 	{
 		if(doCheckpointAnimation)
-			checkpointYIncrementer += (18.0f * Time.deltaTime);
+			checkpointYIncrementer += (12.0f * Time.deltaTime);
 	}
 	void StopFloating()
 	{
